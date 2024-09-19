a18352284a2f3fc4d85032830962b489fd62585f
Marc Hermann <76454594+marc-at-brightnight@users.noreply.github.com>
2024-09-19 14:13:49 -0400

    chore: Add semantic release package (#1)

--- commit-check.sh
+++ commit-check.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-# Get the current branch name
+# Check if this is the first commit on the branch
 branch_name=$(git rev-parse --abbrev-ref HEAD)
-
-# Check the number of commits on this branch that are not on the main branch
 commit_count=$(git rev-list --count main..$branch_name)
 
-# Only check the commit message if it's the first commit on this branch
 if [ "$commit_count" -eq 0 ] || [ "$commit_count" -eq 1 ]; then
-  # Get the commit message of the staged commit
   commit_msg=$(git log -1 --pretty=%B)
 
-  # Define a regex pattern for conventional commit messages
+  # Conventional commit pattern with optional scope
   pattern="^(feat|fix|docs|style|refactor|test|chore|revert|build|ci|perf|other)(\([a-zA-Z0-9_\-]+\))?: .+"
 
-  # Check if the commit message matches the pattern
   if ! echo "$commit_msg" | grep -Eq "$pattern"; then
     echo "Error: The first commit message on this branch does not follow the conventional commit pattern." >&2
     echo "Expected format: <type>(<optional scope>): <description>" >&2
@@ -23,5 +18,4 @@ if [ "$commit_count" -eq 0 ] || [ "$commit_count" -eq 1 ]; then
   fi
 fi
 
-# Allow the commit if all checks pass
 exit 0
